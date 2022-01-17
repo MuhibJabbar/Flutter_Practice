fc19ccc9e8be2fa4439e0c8ad6465a5b56d6ef62
Muhib_Jabbar <muhibjabbar123@gmail.com>
2022-01-17 23:56:16 +0500

    day9 completed

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl =
+        "https://media-exp1.licdn.com/dms/image/C5603AQHt4cAQNQG_Aw/profile-displayphoto-shrink_200_200/0/1544418559700?e=1646870400&v=beta&t=gzHVenwPWhnwJsYsHDKu2K0b38cFP6dIfMBUQFk_flQ";
+
+    return Drawer(
+      child: Container(
+        color: Colors.deepOrange,
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  accountName: Text("Muhib Jabbar"),
+                  accountEmail: Text("muhibjabbar123@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageUrl),
+                  )),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(color: Colors.white),
+                textScaleFactor: 1.2,
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                style: TextStyle(color: Colors.white),
+                textScaleFactor: 1.2,
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email Me",
+                style: TextStyle(color: Colors.white),
+                textScaleFactor: 1.2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
